0c2b0e6bb9232ee496cd6a5a8c5450ff7329f283
Arushi152 <153858396+Arushi152@users.noreply.github.com>
2024-09-11 17:11:18 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score= "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+let Score= Number(score)
+
+console.log(typeof Score)
+console.log(Score);
+
+// "33" => 33
+// "33abc"=> NaN
+// true=> 1, false=> 0
+//null=> 0
+
+let isLoggedIn= 1
+
+let IsLoggedIn= Boolean(isLoggedIn)
+
+console.log(typeof IsLoggedIn);
+console.log(IsLoggedIn);
+
+//1 => true, 0 => false
+//"" => false, "string" => true
+//null, undefined, undeclared => false
+
+
+
+
+
